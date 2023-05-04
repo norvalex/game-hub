af32c7e8ff3aedf1205f87778692d8f791f046b9
Alex <norvalex@users.noreply.github.com>
2023-05-04 19:19:31 +0200

    Filter games by genre

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,13 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App";
 import theme from "./theme";
-
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-        <App />
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
     </ChakraProvider>
   </React.StrictMode>
 );
