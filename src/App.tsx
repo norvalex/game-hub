8285a17a92aac9e822890bfddf8d500b89417831
Alex <norvalex@users.noreply.github.com>
2023-05-04 19:23:25 +0200

    Highlight the selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,7 @@ import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 
-
 function App() {
-
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
 
   return (
@@ -17,8 +15,8 @@ function App() {
         lg: `"nav nav" "aside main"`,
       }}
       templateColumns={{
-        base: '1fr',
-        lg: '200px 1fr'
+        base: "1fr",
+        lg: "200px 1fr",
       }}
     >
       <GridItem area="nav">
@@ -26,7 +24,10 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5}>
-          <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)}/>
+          <GenreList
+            selectedGenre={selectedGenre}
+            onSelectGenre={(genre) => setSelectedGenre(genre)}
+          />
         </GridItem>
       </Show>
       <GridItem area="main">
