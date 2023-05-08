487750ee16100f1ca496287e09dbfda8b4d50d1d
Alex <norvalex@users.noreply.github.com>
2023-05-08 14:13:41 +0200

    No change reformatting

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,12 +1,12 @@
-import genres from  '../data/genres'
+import genres from "../data/genres";
 
 export interface Genre {
-    id: number;
-    name: string;
-    slug: string;
-    image_background: string;
+  id: number;
+  name: string;
+  slug: string;
+  image_background: string;
 }
 
-const useGenres = () => ({ data: genres, isLoading: false, error: null});
+const useGenres = () => ({ data: genres, isLoading: false, error: null });
 
 export default useGenres;
